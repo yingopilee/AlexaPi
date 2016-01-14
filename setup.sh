771ffbbd45652d5147976304e6197c941b56d0af
novaspirit <x3maniac@gmail.com>
2016-01-14 14:01:07 -0500

    fixed setup.sh
    added variable to working director
    fixed IP with extra space
    fixed ifconfig to excute command
    moved echo "open http://$ip:5000" up 2 lines

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
+baselocation = "{$PWD}"
 apt-get update
-apt-get install  libasound2-dev memcached python-pip mpg123 python-alsaaudio python-aubio pip-install
+apt-get install  libasound2-dev memcached python-pip mpg123 python-alsaaudio python-aubio
 pip install -r requirements.txt
 cp initd_alexa.sh /etc/init.d/alexa
 cd /etc/rc5.d
 ln -s ../init.d/alexa S99alexa
 touch /var/log/alexa.log
-
+cd $baselocation
 echo "Enter your ProductID:"
 read productid
 echo ProductID = \"$productid\" >> creds.py
@@ -28,9 +29,9 @@ echo "Enter your Security Client Secret:"
 read secret
 echo Client_Secret = \"$secret\" >> creds.py
 
-ip = `ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
-python ./auth_web.py 
+ip=$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`)
 echo "Open http://$ip:5000"
+python ./auth_web.py 
 
 echo "You can now reboot"
 
